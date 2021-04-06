8629ab0b4edec720790eac24c31184b3bda8240c
touseef75 <imrantouseef776@gmail.com>
2021-04-06 03:04:01 -0700

    fixed

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,13 @@
- function click(){
-    document.getElementById('inpu').value+=i;
-}
+let screen = document.getElementById("hire");
+function result(i){
+    let x = i;
+    if(x=== "="){
+        screen.value=eval(screen.value)
+    }else if(x === "c"){
+        screen.value= "";
+    }else if(x === "d"){
+        screen.value= "";
+    }else{
+        screen.value+=i;
+    }
+};
